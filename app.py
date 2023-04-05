36563450f27c87ff2bb35336da651dcf7a635a38
Nandan Joshi <njoshi@redhat.com>
2023-04-05 09:07:20 -0700

    changed to Espanol

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Chat 🐱")
-option_b = os.getenv('OPTION_B', u"Dog 🐶")
+option_a = os.getenv('OPTION_A', u"Gato 🐱")
+option_b = os.getenv('OPTION_B', u"Perro 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
